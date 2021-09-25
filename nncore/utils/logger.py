db7ff3c81e7c145be376ce25e4117f141f88b356
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2021-09-25 18:48:31 +0700

    move show images to cpu

--- nncore/utils/logger.py
+++ nncore/utils/logger.py
@@ -27,7 +27,7 @@ class TensorboardLogger:
 def image_batch_show(batch, ncol=5, fig_size=(30, 10)):
     # https://stackoverflow.com/questions/51329159/how-can-i-generate-and-display-a-grid-of-images-in-pytorch-with-plt-imshow-and-t
     grid_img = torchvision.utils.make_grid(batch, nrow=ncol)
-    return grid_img.float()
+    return grid_img.float().cpu()
 
 
 def image_batch_show_with_title(batch, title=[], ncol=5, fig_size=(30, 10), show=False):
