27ad96d65558d3a6ed7902100eb0fb590ecf858d
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2021-10-05 03:43:12 +0700

    add sth for visualize segmentation

--- nncore/utils/segmentation.py
+++ nncore/utils/segmentation.py
@@ -1,6 +1,7 @@
 import torch
 from .typing import *
 import numpy as np
+from .color import color_map
 
 
 def multi_class_prediction(output: Tensor) -> Tensor:
@@ -11,20 +12,23 @@ def binary_prediction(output: Tensor, thresh: float = 0.5) -> Tensor:
     return (output.squeeze(1) > thresh).long()
 
 
-class OneHotEncoding:
-    r"""Convert (H,W) to (N_CLASSES, H, W)
+def np2cmap(np_image):
     """
+    input: numpy batch B x H x W
+    output: color map batch  B x C x H x W
+    """
+    cmap = color_map()[:, np.newaxis, :]
+    new_im = np.dot(np_image == 0, cmap[0])
+    cmap = color_map()[:, np.newaxis, :]
+    for i in range(1, cmap.shape[0]):
+        new_im += np.dot(np_image == i, cmap[i])
+    return new_im
 
-    def __init__(self, n_classes: int):
-        super().__init__()
-        self.n_classes = n_classes
 
-    def __call__(self, inputs: np.ndarray) -> np.ndarray:
-        ncols = self.n_classes
-
-        inputs = inputs.astype(int)
-        out = np.zeros((inputs.size, ncols), dtype=np.uint8)
-        out[np.arange(inputs.size), inputs.ravel()] = 1
-        out.shape = inputs.shape + (ncols,)
-        return out
-
+def tensor2cmap(tensor):
+    """
+    input: Tensor batch B x H x W
+    output: color map batch  B x C x H x W
+    """
+    np_image = tensor.permute(1, 2, 0).cpu().numpy()
+    return torch.tensor(np2cmap(np_image).transpose(2, 0, 1))
