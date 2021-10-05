fa0e86d7ff5c25caf6d0a97f21821277cd9427dd
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2021-10-05 09:10:12 +0700

    add description for segmentation cmap convert

--- nncore/utils/segmentation.py
+++ nncore/utils/segmentation.py
@@ -14,9 +14,17 @@ def binary_prediction(output: Tensor, thresh: float = 0.5) -> Tensor:
 
 def np2cmap(np_image):
     """
+    Using Dot product to compute the color batch 
+
+    1. Create a new color axis from H x W x B to H x W x B x 1 
+    2. Multiply the color axis by the image tensor H x W x B x 1 and 1 x C (3 with RGB)
+    3. Sum the color axis to get the final color map H x W x B x C 
+
+
     input: numpy batch H x W x B 
-    output: color map batch  H x W x C x B
+    output: color map batch  H x W x B x C
     """
+
     cmap = color_map()[:, np.newaxis, :]
     np_image = np_image[..., np.newaxis]
     new_im = np.dot(np_image == 0, cmap[0])
